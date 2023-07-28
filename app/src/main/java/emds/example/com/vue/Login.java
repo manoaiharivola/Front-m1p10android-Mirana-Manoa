8149a56ca26e3f8bfc9c9dde3b2d722727a9a2a3
manoaiharivola <manoaiharivola@gmail.com>
2023-07-28 21:15:26 +0300

    splash screen

---
+++ app/src/main/java/emds/example/com/vue/Login.java
@@ -0,0 +1,16 @@
+package emds.example.com.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import emds.example.com.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/emds/example/com/vue/MainActivity.java
+++ app/src/main/java/emds/example/com/vue/MainActivity.java
@@ -2,15 +2,29 @@ package emds.example.com.vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import emds.example.com.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 3500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
