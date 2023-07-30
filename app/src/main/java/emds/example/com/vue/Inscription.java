1b8f6ce58ac5e0526514e2052db7767323834522
manoaiharivola <manoaiharivola@gmail.com>
2023-07-30 23:44:02 +0300

    inscription interface

---
+++ app/src/main/java/emds/example/com/vue/Inscription.java
@@ -0,0 +1,18 @@
+package emds.example.com.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import emds.example.com.R;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_inscription);
+    }
+}
