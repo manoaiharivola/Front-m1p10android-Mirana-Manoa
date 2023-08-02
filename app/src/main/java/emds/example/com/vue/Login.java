85f4dffc94dd7ad2b5041c32715c01a7ce58e93c
manoaiharivola <manoaiharivola@gmail.com>
2023-08-02 21:16:07 +0300

    redirect from login to inscription

--- app/src/main/java/emds/example/com/vue/Login.java
+++ app/src/main/java/emds/example/com/vue/Login.java
@@ -1,18 +1,24 @@
 package emds.example.com.vue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import emds.example.com.R;
 
 public class Login extends AppCompatActivity {
-
+    Button btnCreerUnCompte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        btnCreerUnCompte = (Button) findViewById(R.id.button3);
+        btnCreerUnCompte.setOnClickListener((v) -> {
+            startActivity(new Intent(Login.this, Inscription.class));
+        });
     }
 }
